a21b92ab12ce14641f4d82906ed8523a82916731
Jian yang <fsword73@hotmail.com>
2021-01-28 20:34:08 +0800

    Add 1)Program Unroll;   2) Add Shared Memory to have 2X data read of L1 Cache

--- conv1d.cpp
+++ conv1d.cpp
@@ -154,6 +154,282 @@ __global__ void conv1d_2048_opt1(const float* a, const float* w, float* __restri
 }
 
 
+__global__ void conv1d_2048_opt2(const float* a, const float* w, float* __restrict__ r){
+  int x = hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x;
+  int gid = x * 4;
+  if( gid >= (WIDTH*HEIGHT)){
+     return ;
+  }
+
+
+  float sum[4];
+  float ww[8];
+  float aa[4];
+  for(int i=0; i <4; i++)
+  {
+      sum[i] = 0;
+      ww[i] = w[i];
+      aa[i] = a[i+gid];
+
+  }
+
+  for(int i=0; i < 4; i++)
+  {
+      for(int j=0; j < (4-i);j++)
+      {
+         sum[i] += aa[j+i] * ww[j];
+      }
+       
+  }
+
+  for(int i=4; i < 8; i+=4)
+  {
+     for(int s=0; s<4; s++)
+     {
+       aa[s] = a[gid+i+s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }
+     
+  }
+
+#pragma unroll
+  for(int i=8; i < 2048; i+=4)
+  {
+     for(int s=0; s<4; s++)
+     {
+       aa[s] = a[gid+i+s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }
+
+
+     i+=4;
+     for(int s=0; s<4; s++)
+     {
+       aa[s] = a[gid+i+s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }
+     
+  }
+
+  //last 3
+  for(int i=0; i <3; i++){
+    aa[i] = a[gid+2048+i];   
+  }
+#if 0  
+  sum[1] += aa[0] * ww[3];
+  sum[2] += aa[0] * ww[2];
+  sum[2] += aa[1] * ww[3];
+  sum[3] += aa[0] * ww[1];
+  sum[3] += aa[1] * ww[2];
+  sum[3] += aa[2] * ww[3];
+#else 
+  for(int s=0; s < 3; s++)
+  {
+      for(int t=0; t <(s+1); t++)
+      {
+        sum[s+1] += aa[t] * ww[3-s+t];
+      }
+  }
+#endif 
+
+   for(int i=0; i <4; i++){
+       r[gid+i] = sum[i];
+   } 
+}
+
+//workgroup size = 256
+//LOAD all data into LDS 
+//LDS provides 2X bandwidth vs L1 Cache
+
+__global__ void conv1d_2048_opt3(const float* a, const float* w, float* __restrict__ r){
+  int x = hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x;
+  int gid = x * 4;
+  if( gid >= (WIDTH*HEIGHT)){
+     return ;
+  }
+  float sum[4];
+  float ww[8];
+  float aa[4];
+  const int F_SIZE = 2048;
+  const int WK_SIZE = 256;
+  const int OUT_PIX_SIZE = 4;
+  __shared__ float aData[F_SIZE+WK_SIZE+OUT_PIX_SIZE]; 
+
+  //use global load dword x4
+  for(int i=0; i < (F_SIZE); i+=256){
+#if 0           
+      aData[i + hipThreadIdx_x * 4 + 0 ] = a[gid + i + hipThreadIdx_x * 4 + 0];
+      aData[i + hipThreadIdx_x * 4 + 1 ] = a[gid + i + hipThreadIdx_x * 4 + 1];
+      aData[i + hipThreadIdx_x * 4 + 2 ] = a[gid + i + hipThreadIdx_x * 4 + 2];
+      aData[i + hipThreadIdx_x * 4 + 3 ] = a[gid + i + hipThreadIdx_x * 4 + 3];
+#else
+    aData[i + hipThreadIdx_x]  = a[gid + i + hipThreadIdx_x];
+#endif 
+  }
+
+  // aData[F_SIZE + hipThreadIdx_x] = a [F_SIZE + hipThreadIdx_x];
+  //last 4 data 
+  if(hipThreadIdx_x < 4){
+    aData[hipThreadIdx_x + F_SIZE+WK_SIZE] =  a[gid + hipThreadIdx_x + F_SIZE+WK_SIZE];
+  }
+
+  for(int i=0; i <4; i++)
+  {
+      sum[i] = 0;
+      ww[i] = w[i];
+      //aa[i] = a[i+gid];
+      aa[i] = aData[i+hipThreadIdx_x];
+
+  }
+
+  //first 8x weight for 1st output pixel
+  //to make sure filter ready
+  for(int i=0; i < 4; i++)
+  {
+      for(int j=0; j < (4-i);j++)
+      {
+         sum[i] += aa[j+i] * ww[j];
+      }
+       
+  }
+
+  for(int i=4; i < 8; i+=4)
+  {
+     for(int s=0; s<4; s++)
+     {
+       //aa[s] = a[gid+i+s];
+       aa[s] = aData[hipThreadIdx_x+i+s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }
+     
+  }
+
+  //first 2040x weight for all output pixel
+#pragma unroll
+  for(int i=8; i < 2048; i+=4)
+  {
+     for(int s=0; s<4; s++)
+     {
+       //aa[s] = a[gid+i+s];
+       aa[s] = aData[hipThreadIdx_x+i+s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }
+
+
+     i+=4;
+     for(int s=0; s<4; s++)
+     {
+       //aa[s] = a[gid+i+s];
+       aa[s] = aData[hipThreadIdx_x + i + s];
+       ww[s+4] = w[i+s];
+     }
+     for(int s =0; s < 4; s++)
+     {
+        int offset = 4-s;
+        for(int t=0; t < 4; t++)
+        {
+          sum[s] += aa[t] * ww[offset + t];
+        }
+     }
+
+     //shift left
+     for(int s=0; s < 4; s++){
+       ww[s] = ww[s+4];
+     }     
+  }
+
+  //rest 3 pixels's rest weight, 
+  for(int i=0; i <3; i++){
+    aa[i] = aData[hipThreadIdx_x + F_SIZE + i];   
+  }
+#if 0  
+  sum[1] += aa[0] * ww[3];
+  sum[2] += aa[0] * ww[2];
+  sum[2] += aa[1] * ww[3];
+  sum[3] += aa[0] * ww[1];
+  sum[3] += aa[1] * ww[2];
+  sum[3] += aa[2] * ww[3];
+#else 
+  for(int s=0; s < 3; s++)
+  {
+      for(int t=0; t <(s+1); t++)
+      {
+        sum[s+1] += aa[t] * ww[3-s+t];
+      }
+  }
+#endif 
+
+   for(int i=0; i <4; i++){
+       r[gid+i] = sum[i];
+   } 
+}
 
 
 
@@ -257,6 +533,60 @@ int main() {
           ips =( double) (NUM-2048) * 2048 * 64 /1024/1024/1024;
           ips = ips /(double)  eventMs * 1000 ;
           printf("conv1d_2048_opt1 ==> %lf G FMAs/s, time: %f \n", ips, eventMs );
+   }
+
+{
+          hipLaunchKernelGGL(conv1d_2048_opt2, 
+                        dim3((WIDTH*HEIGHT)/THREADS_PER_BLOCK_X/4),
+                        dim3(THREADS_PER_BLOCK_X ),
+                        0, 0,
+                        deviceA ,deviceB ,deviceC);
+
+          hipEventRecord(start, NULL);
+        for (int i = 1; i < 64; i++)
+        {
+          hipLaunchKernelGGL(conv1d_2048_opt2, 
+                        dim3((WIDTH*HEIGHT)/THREADS_PER_BLOCK_X/4),
+                        dim3(THREADS_PER_BLOCK_X),
+                        0, 0,
+                        deviceA ,deviceB ,deviceC);
+        }
+          hipEventRecord(stop, NULL);
+          hipEventSynchronize(stop);
+
+          hipEventElapsedTime(&eventMs, start, stop);
+
+          //printf("elapsed time:%f\n", eventMs);
+          ips =( double) (NUM-2048) * 2048 * 64 /1024/1024/1024;
+          ips = ips /(double)  eventMs * 1000 ;
+          printf("conv1d_2048_opt2 ==> %lf G FMAs/s, time: %f \n", ips, eventMs );
+   }
+
+{
+          hipLaunchKernelGGL(conv1d_2048_opt3, 
+                        dim3((WIDTH*HEIGHT)/256/4),
+                        dim3(256 ),
+                        0, 0,
+                        deviceA ,deviceB ,deviceC);
+
+          hipEventRecord(start, NULL);
+        for (int i = 1; i < 64; i++)
+        {
+          hipLaunchKernelGGL(conv1d_2048_opt3, 
+                        dim3((WIDTH*HEIGHT)/256/4),
+                        dim3(256 ),
+                        0, 0,
+                        deviceA ,deviceB ,deviceC);
+        }
+          hipEventRecord(stop, NULL);
+          hipEventSynchronize(stop);
+
+          hipEventElapsedTime(&eventMs, start, stop);
+
+          //printf("elapsed time:%f\n", eventMs);
+          ips =( double) (NUM-2048) * 2048 * 64 /1024/1024/1024;
+          ips = ips /(double)  eventMs * 1000 ;
+          printf("conv1d_2048_opt2 ==> %lf G FMAs/s, time: %f \n", ips, eventMs );
    }
 
   HIP_ASSERT(hipMemcpy(hostA, deviceA, NUM*sizeof(float), hipMemcpyDeviceToHost));
